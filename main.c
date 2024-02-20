0c3fe9c560b523ddce5c30bec9a9126c6acde4ba
Ahmad Byagowi <ahmadexp@gmail.com>
2024-02-19 21:09:06 -0800

    Update main.c

--- main.c
+++ main.c
@@ -34,12 +34,13 @@ int main(int argc, char** argv) {
 
     // mesh_t* shape = obj_mesh_from_file(g_mesh_file, g_cx, g_cy, g_cz, g_width, g_height, g_depth);
     mesh_t* shape = obj_mesh_from_file(object_file, g_cx, g_cy, g_cz, g_cube_size, 1.2*g_cube_size, g_cube_size);
-    
+
+	obj_mesh_translate_by(shape, g_move_x, g_move_y, g_move_z);
+	
 	do{    
 	get_eul(&bnod);
 	
     	obj_mesh_rotate_to(shape,bnod.eul_pitc*M_PI/180,bnod.eul_head*M_PI/180,bnod.eul_roll*M_PI/180);
-		obj_mesh_translate_by(shape, g_move_x, g_move_y, g_move_z);
     	render_write_shape(shape);
     	render_flush();
 #ifndef _WIN32
